c1ddcd03893e8f1214d6d0dbb332c43fb404dd14
jiu-zui-90817 <163388829+jiu-zui-90817@users.noreply.github.com>
2026-08-22 10:43:48 +0800

    Add files via upload

--- Backend/src/pch.h
+++ Backend/src/pch.h
@@ -1,17 +1,13 @@
-/**
- * 最小预编译头 — 若 VS 工程启用「预编译头」则使用本文件。
- * 也可在 dllmain.cpp 中删除 #include "pch.h" 并直接包含下方头。
- */
-#pragma once
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
 
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
+#ifndef PCH_H
+#define PCH_H
 
-#include <windows.h>
-#include <string>
-#include <iostream>
-#include <vector>
-#include <sstream>
-#include <map>
-#include <fstream>
+// 添加要在此处预编译的标头
+#include "framework.h"
+
+#endif //PCH_H
